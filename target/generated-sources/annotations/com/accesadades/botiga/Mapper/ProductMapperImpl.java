24c780608845e3bc612864b794dd3456b2221a17
DanielAG359 <2023_Daniel.Acosta@iticbcn.cat>
2025-04-11 17:13:24 +0200

    Es inecesario por eso lo he quitado, pero me hacia gracia tenerlo ya que lo habia visto

--- target/generated-sources/annotations/com/accesadades/botiga/Mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/com/accesadades/botiga/Mapper/ProductMapperImpl.java
@@ -2,14 +2,9 @@ package com.accesadades.botiga.Mapper;
 
 import com.accesadades.botiga.DTO.ProductDTO;
 import com.accesadades.botiga.Model.Product;
-import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
-@Generated(
-    value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-11T17:09:41+0200",
-    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
-)
+
 @Component
 public class ProductMapperImpl implements ProductMapper {
 
