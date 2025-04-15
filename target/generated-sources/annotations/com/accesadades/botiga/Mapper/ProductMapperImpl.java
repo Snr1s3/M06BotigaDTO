c03e0f4d2e8693940f2eae99dd0d36a8e5c0ffce
DanielAG359 <2023_Daniel.Acosta@iticbcn.cat>
2025-04-15 16:49:51 +0200

    merge del main

--- target/generated-sources/annotations/com/accesadades/botiga/Mapper/ProductMapperImpl.java
+++ target/generated-sources/annotations/com/accesadades/botiga/Mapper/ProductMapperImpl.java
@@ -1,13 +1,15 @@
 package com.accesadades.botiga.Mapper;
 
 import com.accesadades.botiga.DTO.ProductDTO;
+import com.accesadades.botiga.DTO.SubcategoryDTO;
 import com.accesadades.botiga.Model.Product;
+import com.accesadades.botiga.Model.Subcategoria;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-10T08:29:18+0200",
+    date = "2025-04-15T16:47:34+0200",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
@@ -21,7 +23,7 @@ public class ProductMapperImpl implements ProductMapper {
 
         ProductDTO productDTO = new ProductDTO();
 
-        productDTO.setSubcategory( map( product.getSubcategory() ) );
+        productDTO.setSubcategory( subcategoriaToSubcategoryDTO( product.getSubcategory() ) );
         productDTO.setName( product.getName() );
         productDTO.setDescription( product.getDescription() );
         productDTO.setCompany( product.getCompany() );
@@ -39,7 +41,7 @@ public class ProductMapperImpl implements ProductMapper {
 
         Product product = new Product();
 
-        product.setSubcategory( map( productDTO.getSubcategory() ) );
+        product.setSubcategory( subcategoryDTOToSubcategoria( productDTO.getSubcategory() ) );
         product.setCompany( productDTO.getCompany() );
         product.setDescription( productDTO.getDescription() );
         product.setName( productDTO.getName() );
@@ -47,5 +49,29 @@ public class ProductMapperImpl implements ProductMapper {
         product.setUnits( productDTO.getUnits() );
 
         return product;
+    }
+
+    protected SubcategoryDTO subcategoriaToSubcategoryDTO(Subcategoria subcategoria) {
+        if ( subcategoria == null ) {
+            return null;
+        }
+
+        SubcategoryDTO subcategoryDTO = new SubcategoryDTO();
+
+        subcategoryDTO.setName( subcategoria.getName() );
+
+        return subcategoryDTO;
+    }
+
+    protected Subcategoria subcategoryDTOToSubcategoria(SubcategoryDTO subcategoryDTO) {
+        if ( subcategoryDTO == null ) {
+            return null;
+        }
+
+        Subcategoria subcategoria = new Subcategoria();
+
+        subcategoria.setName( subcategoryDTO.getName() );
+
+        return subcategoria;
     }
 }
